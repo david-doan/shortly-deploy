b655348a3afa4cc133353a5a53c6f3af3283a961
Hack Reactor Students <accounts@hackreactor.com>
2016-05-18 19:52:45 -0700

    create mongo schema

--- app/config.js
+++ app/config.js
@@ -1,40 +1,35 @@
-var path = require('path');
-var knex = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, '../db/shortly.sqlite')
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/shortly');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to Shortly mongoDB');
+});
+
+var Schema = mongoose.Schema;
+
+var urls = new Schema({
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number,
+  timestamp: Date
+});
+
+var users = new Schema({
+  username: {
+    type: String,
+    unique: true
   },
-  useNullAsDefault: true
-});
-var db = require('bookshelf')(knex);
-
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+  password: String,
+  timestamp: Date
 });
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+var User = mongoose.model('User', users);
+var Urls = mongoose.model('Urls', urls);
+
 
 module.exports = db;
+module.exports = User;
+module.exports = Urls;
