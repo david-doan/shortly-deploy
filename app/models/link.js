ab6480b991018403f37053c49f89f9d6c89df6e0
Hack Reactor Students <accounts@hackreactor.com>
2016-05-18 21:12:01 -0700

    write links to URLs db

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,42 @@
-var db = require('../config');
+var db = require('../config.js');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
-});
 
-module.exports = Link;
+var shortenUrl = function(url) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  return shasum.digest('hex').slice(0, 5);
+};
+
+
+var makeLink = function(obj) {
+  var newLink = new db.urlModel({
+    url: obj.url,
+    baseUrl: obj.baseUrl,
+    title: obj.title,
+    visits: 0, //need a way to increment
+    timestamp: Date.now()
+  });
+
+  newLink.code = shortenUrl(obj.url);
+
+  return newLink;
+};
+
+// var test = makeLink({
+//   url: 'www.hackreactor.com',
+//   baseUrl: 'www.hackreactor.com',
+//   title: 'Hack Reactor',
+// });
+
+// test.save( (err) => {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log('SUCCESS LINK CREATION');
+// });
+
+
+
+module.exports = makeLink;
